b44354c62defdace1e88e0d845d81b5fab61f5fc
Unknown <citizenkaneVII@gmail.com>
2018-01-10 09:53:19 -0700

    Created NumberGuesser Project

---
+++ NumberGuesser/NumberGuesser/Assets/Guesser.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guesser : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("Our Game has started...  Infidel.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
