075240c4f8f25ecc3fae1112c4c8b5f2706a3079
Unknown <citizenkaneVII@gmail.com>
2018-01-12 11:23:22 -0700

    Update 1/12/2018

--- DGM-1600-002/NumberGuesser/Assets/Guesser.cs
+++ DGM-1600-002/NumberGuesser/Assets/Guesser.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class Guesser : MonoBehaviour {
 
+		public int max;
+		public int min;
+		public int guess;
+
 	// Use this for initialization
-	void Start () {
-		print("Our Game has started...  Infidel.");
+	private void Start () {
+		print("Welcome to number guesser");
+		print("Pick a number between " +min +" and " +max);
+		
+		//Is the value GUESS
+		print("Is the number 500?");
+		//Instructions - Push these buttons
+		print("Up arrow for higher, Down arrow for lower, Enter for correct");
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	public void Update () {
+		print(max);
 	}
 }
