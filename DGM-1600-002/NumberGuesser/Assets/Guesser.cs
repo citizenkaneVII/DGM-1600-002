48054110094be8a3e58706675eb0d34c757820f4
Unknown <citizenkaneVII@gmail.com>
2018-01-17 09:48:11 -0700

    Update 1/17/2018
    (Get.KeyDown(KeyCode.Up)) function

--- DGM-1600-002/NumberGuesser/Assets/Guesser.cs
+++ DGM-1600-002/NumberGuesser/Assets/Guesser.cs
@@ -2,26 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Guesser : MonoBehaviour {
 
 		public int max;
 		public int min;
 		public int guess;
 
+
 	// Use this for initialization
-	private void Start () {
-		print("Welcome to number guesser");
-		print("Pick a number between " +min +" and " +max);
+	private void Start () 
+	{
+		print ("Welcome to number guesser");
+		print ("Pick a number between " +min +" and " +max);
 		
 		//Is the value GUESS
-		print("Is the number 500?");
+		print ("Is the number " + guess + "?");
 		//Instructions - Push these buttons
-		print("Up arrow for higher, Down arrow for lower, Enter for correct");
-
+		print ("Up arrow for higher, Down arrow for lower, Enter for correct");
 	}
 	
+
 	// Update is called once per frame
-	public void Update () {
-		print(max);
+	public void Update () 
+	{
+
+		//Down Arrow command
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			min = guess;
+			guess = (min+max) / 2;
+			print ("Is the number " + guess + "?");
+		}
+
+		//Up Arrow command
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			print ("Do more stuff.");
+		}
+
+		//Enter command
+
 	}
+
+
+
 }
