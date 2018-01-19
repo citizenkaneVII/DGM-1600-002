6cc982e4ae7ae8f793e96f54de96180ab50ade45
Unknown <citizenkaneVII@gmail.com>
2018-01-19 11:11:37 -0700

    Number Guesser Update 1/19/2018

--- DGM-1600-002/NumberGuesser/Assets/Guesser.cs
+++ DGM-1600-002/NumberGuesser/Assets/Guesser.cs
@@ -6,51 +6,101 @@ using UnityEngine;
 public class Guesser : MonoBehaviour {
 
 		public int max;
+		private int maxsave;
 		public int min;
+		private int minsave;
 		public int guess;
+		public int count;
+		private int countsave;
 
 
 	// Use this for initialization
 	private void Start () 
 	{
-		print ("Welcome to number guesser");
+		max = max + 1;
+		maxsave = max;
+		minsave = min;
+		countsave = count;
+
+		print ("Welcome to Number Guesser, dingus.");
 		print ("Pick a number between " + min + " and " + max);
 		
-		//Is the value GUESS
-		print ("Is the number " + guess + "?");
+		//Is the value NextGuess
+		NextGuess();
+
 		//Instructions - Push these buttons
-		print ("Up arrow for higher, Down arrow for lower, Enter for correct");
+		print ("Press Up arrow for higher, Down arrow for lower, Enter for correct.  Think you got that?");
+	}
+
+
+
+	//NextGuess function
+	private void NextGuess()
+	{
+		count = count - 1; //Count - 1
+		//figure out how to randomize guess********************
+		guess = (min + max) / 2;
+		print ("Is the number " + guess + "?");
 	}
 	
+
+
+	//Reset function
+	private void Restart()
+	{
+				max = maxsave;
+				min = minsave;
+				count = countsave;
+				Start();
+	}
+
+
 
 	// Update is called once per frame
 	public void Update () 
 	{
 
+		if (count == 0)
+		{
+			print("You Suck! I Hate You! YOU Win...");
+		}
+
+		else if (guess == 666)
+		{
+			print ("Hail Satan.");
+		}
+
 		//Down Arrow command
-		if (Input.GetKeyDown(KeyCode.UpArrow))
+		else if (Input.GetKeyDown(KeyCode.UpArrow))
 		{
 			min = guess;
-			guess = (min + max) / 2;
-			print ("Is the number " + guess + "?");
+			NextGuess();
 		}
+
 
 		//Up Arrow command
-		if (Input.GetKeyDown(KeyCode.DownArrow))
+		else if (Input.GetKeyDown(KeyCode.DownArrow))
 		{
 			max = guess;
-			guess = (min + max) / 2;
-			print ("Is the number " + guess + "?" );
+			NextGuess();
 		}
 
-		if(Input.GetKeyDown(KeyCode.KeypadEnter))
-		{
-			print("I win!");
-		}
 
 		//Enter command
+		else if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+		{	
+			print("HA HA LOSER, I WIN!");
+			print("Press R to play again, dweb?");
+			
+		}
+
+		else if (Input.GetKeyDown(KeyCode.R))
+		{
+			Restart();
+		}
 
 	}
+
 
 
 
