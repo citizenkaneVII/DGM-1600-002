55b173924cb51b4600bb8f66ba222cfdfbb5a314
Unknown <citizenkaneVII@gmail.com>
2018-02-05 10:10:25 -0700

    BrickBreakerUpdate01

---
+++ BrickBreaker/Assets/Scripts/DontDestory.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestory : MonoBehaviour {
+
+	void Awake() {
+		DontDestroyOnLoad(transform.gameObject);
+	}
+}
