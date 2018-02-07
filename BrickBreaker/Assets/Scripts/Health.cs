361c50faa64a482aeea6856f58cc0a3a299f56f2
Unknown <citizenkaneVII@gmail.com>
2018-02-07 10:39:39 -0700

    bRICK bREAKER 2

---
+++ BrickBreaker/Assets/Scripts/Health.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+	public int health;
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		health--;
+		if (health <= 0)
+		{
+			Destroy (gameObject);
+		}
+	}
+}
