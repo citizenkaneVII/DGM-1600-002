8566808cda92077342f9d5a77a16c320dab16ef7
Unknown <citizenkaneVII@gmail.com>
2018-02-09 09:58:32 -0700

    Brick Breaker 2/9/18

---
+++ BrickBreaker/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+	public static MusicManager instance = null;
+
+	void Awake() 
+	{
+		//singlton
+		if (instance == null)		//if instance not assigned
+		{
+			instance = this;		//then assign instance to this object
+		}
+		else if (instance!= this)
+		{
+			Destroy(this.gameObject);	//then destroy this object
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
