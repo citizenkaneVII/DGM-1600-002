5742c6ffe6d26dc1bdd4f7eab1d41d8e6bdc4251
Unknown <citizenkaneVII@gmail.com>
2018-02-12 10:39:54 -0700

    Brkbrkr

---
+++ BrickBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+	
+	public float verticalOffset;
+	//
+
+
+	void Update ()
+	{
+		//get currenet paddle position
+		Vector3 paddlePosition = new Vector3(0, -5, 10);
+		//set verticle off set
+
+		//get mouse position
+		float mousePositionInBlocks = Input.mousePosition.x / Screen.width * 16 - 8;
+
+		//convert mouse position to world space
+		//save new position
+		paddlePosition.x = mousePositionInBlocks;
+
+		//set this paddle object to saved position	
+		this.gameObject.transform.position = paddlePosition;
+	}
+}
