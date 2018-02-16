a2f41ecc4e58088dd816733b772265d1ef6e1561
Unknown <citizenkaneVII@gmail.com>
2018-02-16 09:58:24 -0700

    2/16/2018 Brick Breaker

---
+++ BrickBreaker/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+public static GameManager instance = null;
+
+	void Awake() 
+	{
+		//singlton
+		if (instance == null)		//if instance not assigned
+		{
+			instance = this;		//then assign instance to this object
+		}
+		else if (instance != this)
+		{
+			Destroy(this.gameObject);	//then destroy this object
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+
+public void LoadLevel (string level) 
+	{
+		SceneManager.LoadScene(level);
+	}
+}
