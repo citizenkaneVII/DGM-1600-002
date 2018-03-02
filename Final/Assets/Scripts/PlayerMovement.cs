f3a22c448dfa39501343e108dff69e93804e5ece
Unknown <citizenkaneVII@gmail.com>
2018-03-02 09:52:25 -0700

    Update-Final-3/2/18

---
+++ Final/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+
+	public Rigidbody2D rigid;
+	public float speed;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		rigid = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//check for button pushes
+		rigid.AddForce(new Vector2(Input.GetAxis("Horizontal") * speed, 0), ForceMode2D.Force);
+
+	}
+}
