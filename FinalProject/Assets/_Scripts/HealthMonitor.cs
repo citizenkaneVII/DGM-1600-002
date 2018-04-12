59f59903860b2b596f3b06aaea56dcfb73a1d42f
Unknown <citizenkaneVII@gmail.com>
2018-04-12 17:41:07 -0600

    Lesson05

---
+++ FinalProject/Assets/_Scripts/HealthMonitor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthMonitor : MonoBehaviour {
+
+	public static int HealthValue;
+	public int InternalHealth;
+	public GameObject Heart01;
+	public GameObject Heart02;
+	public GameObject Heart03;
+
+	// Use this for initialization
+	void Start () {
+		HealthValue = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		InternalHealth = HealthValue;
+
+		if(HealthValue == 1){
+			Heart01.SetActive (true);
+		}
+		if(HealthValue == 2){
+			Heart02.SetActive (true);
+		}
+		if(HealthValue == 3){
+			Heart03.SetActive (true);
+		}
+
+
+
+	}
+}
--- FinalProject/Assets/_Scripts/HeartCollect.cs
+++ FinalProject/Assets/_Scripts/HeartCollect.cs
@@ -17,6 +17,7 @@ public class HeartCollect : MonoBehaviour {
 
 	void OnTriggerEnter () {
 		CollectSound.Play ();
+		HealthMonitor.HealthValue += 1;
 		ThisHeart.SetActive (false);
 	}
 	
