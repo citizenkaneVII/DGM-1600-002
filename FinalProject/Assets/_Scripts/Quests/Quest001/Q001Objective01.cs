04fb212d5c215caf3f836f47a35cd457d9f097ae
Unknown <citizenkaneVII@gmail.com>
2018-04-26 13:21:56 -0600

    009.1

---
+++ FinalProject/Assets/_Scripts/Quests/Quest001/Q001Objective01.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Q001Objective01 : MonoBehaviour {
+
+	public GameObject TheObject;
+	public int CloseObjective;
+
+	void Update () {
+		if (CloseObjective == 1){
+			if (TheObjective.transform.localScale.y <= 0.0f){
+				CloseObjective = 0;
+				TheObjective.SetActive (false);
+			}
+			else {
+				TheObjective.transform.localScale.y -= new Vecotr3 (0.0f, 0.01f, 0.0f);
+			}
+		}
+	}
+
+	void onTriggerEnter(){
+		StartCoroutine (FinishObjective());
+	}
+
+	IEnumerator FinishObjective(){
+		TheObjective.SetActive (true);
+		yield return new WaitForSeconds (0.5f);
+		CloseObjective = 1;
+
+	}
+
+}
