61aa2074123c5ae95d66c098018121f5aa5f95d2
Unknown <citizenkaneVII@gmail.com>
2018-04-26 18:27:39 -0600

    010

--- FinalProject/Assets/_Scripts/Quests/Quest001/Q001Objective01.cs
+++ FinalProject/Assets/_Scripts/Quests/Quest001/Q001Objective01.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class Q001Objective01 : MonoBehaviour {
 
-	public GameObject TheObject;
+	public GameObject TheObjective;
 	public int CloseObjective;
 
 	void Update () {
@@ -15,16 +15,16 @@ public class Q001Objective01 : MonoBehaviour {
 				TheObjective.SetActive (false);
 			}
 			else {
-				TheObjective.transform.localScale.y -= new Vecotr3 (0.0f, 0.01f, 0.0f);
+				TheObjective.transform.localScale -= new Vector3 (0.0f, 0.01f, 0.0f);
 			}
 		}
 	}
 
 	void onTriggerEnter(){
-		StartCoroutine (FinishObjective());
+		StartCoroutine (FinishObjective ());
 	}
 
-	IEnumerator FinishObjective(){
+	IEnumerator FinishObjective (){
 		TheObjective.SetActive (true);
 		yield return new WaitForSeconds (0.5f);
 		CloseObjective = 1;
